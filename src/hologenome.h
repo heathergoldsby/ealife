e34c77477c3ce250dd2153656719ebb5f2886705
Heather Goldsby <heather.goldsby@gmail.com>
2013-07-29 08:44:01 -0400

    hologenome - not nand balance code

--- src/hologenome.h
+++ src/hologenome.h
@@ -80,5 +80,56 @@ struct not_strength : periodic_event<METAPOP_COMPETITION_PERIOD,EA> {
     datafile _df;
 };
 
+/*! Competition to maximize the product of nots and nands.
+ */
+
+LIBEA_MD_DECL(NOT_NAND_PROD, "ea.hologenome.not_nand_prod", double);
+
+
+template <typename EA>
+struct not_nand_balance : periodic_event<METAPOP_COMPETITION_PERIOD,EA> {
+    not_nand_balance(EA& ea) : periodic_event<METAPOP_COMPETITION_PERIOD,EA>(ea), _df("not_nand_balance.dat") {
+        _df.add_field("update")
+        .add_field("mean_fitness")
+        .add_field("max_fitness");
+    }
+    
+    virtual ~not_nand_balance() {
+    }
+    
+    virtual void operator()(EA& ea) {
+        using namespace boost::accumulators;
+        accumulator_set<double, stats<tag::mean, tag::max> > fit;
+        
+        // calculate "fitness":
+        for(typename EA::population_type::iterator i=ea.population().begin(); i!=ea.population().end(); ++i) {
+            double nn = get<TASK_NOT>(**i,0) * get<TASK_NAND>(**i,0);
+            put<NOT_NAND_PROD>(nn, **i);
+            fit(nn);
+        }
+        
+        _df.write(ea.current_update())
+        .write(mean(fit))
+        .write(max(fit))
+        .endl();
+        
+        std::size_t n=get<META_POPULATION_SIZE>(ea);
+        typename EA::population_type offspring;
+        recombine_n(ea.population(), offspring,
+                    selection::tournament < access::meta_data<NOT_NAND_PROD> > (n, ea.population(), ea),
+                    recombination::propagule_without_replacement(),
+                    n, ea);
+        
+        // mutate? if desired?
+        
+        // swap populations
+        std::swap(ea.population(), offspring);
+        
+        
+    }
+    
+    datafile _df;
+};
+
 
 #endif
