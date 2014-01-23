6279d5cf9389c042c128df9028d2614d503df27a
Dave Knoester <dave.knoester@gmail.com>
2014-01-22 23:17:29 -0500

    added analysis tool to pretty print the last genome of an LOD

--- src/main.cpp
+++ src/main.cpp
@@ -223,7 +223,7 @@ public:
         add_tool<ea::analysis::lod_gls_aging_res_over_time>(this);
         add_tool<ea::analysis::lod_gls_aging_res_over_time_compact>(this);
         add_tool<ea::analysis::lod_gls_task_count>(this);
-        
+        add_tool<ea::analysis::lod_last_genome>(this);        
         
     }
     
